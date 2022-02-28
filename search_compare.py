b8cf461f1587750bfafcc95b3b0f65ea95ba8f1e
Tech Kaya <74741855+techkayacodes@users.noreply.github.com>
2022-02-27 21:35:28 -0500

    Overriding master files for IS211_Assignment 4

--- search_compare.py
+++ search_compare.py
@@ -1,35 +1,90 @@
 import argparse
-# other imports go here
-
+import time
 import random
 
-def get_me_random_list(n):
-    """Generate list of n elements in random order
-    
-    :params: n: Number of elements in the list
-    :returns: A list with n elements in random order
-    """
-    a_list = list(range(n))
-    random.shuffle(a_list)
-    return a_list
+def sequential_search(arr,ele):
+    pos = 0
+    found = False
+    stopped = False
+    while pos < len(arr) and not found and not stopped:
+        if arr[pos] == ele:
+            found = True     
+        else:
+            if arr[pos] > ele:
+                stopped = True
+            else:
+                pos  = pos+1   
+    return found
+
+def ordered_sequential_search(ordered_list, term): 
+    ordered_list_size = len(ordered_list) 
+    for i in range(ordered_list_size): 
+        if term == ordered_list[i]: 
+            return i 
+        elif ordered_list[i] > term: 
+            return None 
+    return None
+
+def binary_search_iterative(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+
+def binary_search_recursive(arr, target):
+    mid = len(arr) // 2
+    if len(arr) == 1:
+        return mid if arr[mid] == target else None
+    elif arr[mid] == target:
+        return mid
+    else:
+        if arr[mid] < target:
+            callback_response = binary_search_recursive((arr[mid:]), target)
+            return mid + callback_response if callback_response is not None else None
+        else:
+            return binary_search_recursive((arr[:mid]), target)
 
 
-def sequential_search(a_list,item):
-    pass
+main_list=[]
+
+for i in range(100):
+  main_list.append(random.sample(range(1,1000),500))
 
 
-def ordered_sequential_search(a_list,item):
-    pass
+for arr in main_list:
+  ele = random.randint(1000,2000)
 
+  begin = time.time()
+  sequential_search(arr,ele)
+  end = time.time()
+  print(f"Total runtime of the sequential search is {end - begin}")
 
-def binary_search_iterative(a_list,item):
-    pass
-    
-    
-def binary_search_recursive(a_list,item):
-    pass
+  begin = time.time()
+  ordered_sequential_search(arr,ele)
+  end = time.time()
+  print(f"Total runtime of the ordered sequential search is {end - begin}")
 
+  begin = time.time()
+  binary_search_iterative(arr,ele)
+  end = time.time()
+  print(f"Total runtime of the binary search iterative is {end - begin}")
+
+  begin = time.time()
+  binary_search_recursive(arr,ele)
+  end = time.time()
+  print(f"Total runtime of the binary search recursive is {end - begin}")
 
 if __name__ == "__main__":
     """Main entry point"""
     pass
+
+
+#Comment
--- sort_compare.py
+++ sort_compare.py
@@ -1,90 +1,50 @@
 import argparse
-# other imports go here
-
-import random
 import time
+import random
 
-def get_me_random_list(n):
-    """Generate list of n elements in random order
-    
-    :params: n: Number of elements in the list
-    :returns: A list with n elements in random order
-    """
-    a_list = list(range(n))
-    random.shuffle(a_list)
-    return a_list
-    
+def insertionSort(array):
+    for step in range(1, len(array)):
+        key = array[step]
+        j = step - 1      
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
+            j = j - 1
+        array[j + 1] = key
 
-def insertion_sort(a_list):
-    for index in range(1, len(a_list)):
-        current_value = a_list[index]
-        position = index
-
-        while position > 0 and a_list[position - 1] > current_value:
-            a_list[position] = a_list[position - 1]
-            position = position - 1
-
-        a_list[position] = current_value
-
-
-def shellSort(alist):
-    sublistcount = len(alist)//2
-    while sublistcount > 0:
-        for startposition in range(sublistcount):
-            gapInsertionSort(alist,startposition,sublistcount)
-
-        print("After increments of size", sublistcount, "The list is",alist)
-
-        sublistcount = sublistcount // 2
+def shellSort(array, n):
+    interval = n // 2
+    while interval > 0:
+        for i in range(interval, n):
+            temp = array[i]
+            j = i
+            while j >= interval and array[j - interval] > temp:
+                array[j] = array[j - interval]
+                j -= interval
+            array[j] = temp
+        interval //= 2
 
 
-def gapInsertionSort(alist, start, gap):
+main_list=[]
 
-    for i in range(start+gap, len(alist), gap):
-        currentvalue = alist[i]
-        position = i
-
-        while position >= gap and alist[position-gap] > currentvalue:
-            alist[position] = alist[position-gap]
-            position = position - gap
-
-        alist[position] = currentvalue
+for i in range(100):
+  main_list.append(random.sample(range(1,1000),500))
 
 
-def python_sort(a_list):
-    """
-    Use Python built-in sorted function
+for arr in main_list:
+  
+  begin = time.time()
+  insertionSort(arr)
+  end = time.time()
+  print(f"Total runtime of the insertiion sort is {end - begin}")
 
-    :param a_list:
-    :return: the sorted list
-    """
-    return sorted(a_list)
+  begin = time.time()
+  shellSort(arr,10)
+  end = time.time()
+  print(f"Total runtime of the shell sort is {end - begin}")
 
+  begin = time.time()
+  arr.sort()
+  end = time.time()
+  print(f"Total runtime of the python sort is {end - begin}")
 
-if __name__ == "__main__":
-    """Main entry point"""
-    list_sizes = [500, 1000, 5000]
-
-    the_size = list_sizes[0]
-
-    total_time = 0
-    for i in range(100):
-        mylist500 = get_me_random_list(the_size)
-        start = time.time()
-        sorted_list = python_sort(mylist500)
-        time_spent = time.time() - start
-        total_time += time_spent
-
-    avg_time = total_time / 100
-    print(f"Python sort took {avg_time:10.7f} seconds to run, on average for a list of {the_size} elements")
-
-    total_time = 0
-    for i in range(100):
-        mylist500 = get_me_random_list(the_size)
-        start = time.time()
-        insertion_sort(mylist500)
-        time_spent = time.time() - start
-        total_time += time_spent
-
-    avg_time = total_time / 100
-    print(f"Insertion sort took {avg_time:10.7f} seconds to run, on average for a list of {the_size} elements")
+  #Comment
